5e7555af9980438d3559e5183c886654f24c5e64
Bianca Christiana Savoiu Marinas <biancachristiana.savoiu@mail.polimi.it>
2022-11-12 23:32:29 +0100

    Adjust dataCleaning
    The previous process wasn't working properly. Updated with a new data cleaning keeping the operations in Mongosh and the main filterings, but changing the process used for the 'venue' data. Python code to manage 'venue' and vim substitution to obtain integers. Everything is working, but with the using data after the filtering from 2300 elements we have only 600 elements. Try with a bigger dataset.

---
+++ mongo-db/data_cleaning/pythonCleaning/extractAddVenue.py
@@ -0,0 +1,25 @@
+import json
+
+jsonFile = open('datasetMongo.json', 'r')
+values = json.load(jsonFile)
+
+rawValues = {}
+i = 0
+for line in values:
+    if 'venue' in line:
+        rawValues[i] = line['venue']['raw']
+        i = i + 1
+print(rawValues)
+
+raw = 0
+for element in values:
+    if 'venue' in element:
+        del element['venue']
+    if '_id' in element:
+        element['venue'] = rawValues[raw]
+        raw = raw + 1
+
+with open('addVenue.json', 'w') as jsonF:
+    jsonF.write(json.dumps(values, indent=4))
+
+
