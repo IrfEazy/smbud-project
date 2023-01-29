aa239b4de290bf52d4feecda7b2b99e3760ce9df
Irfan Çela <50823808+irfeazy@users.noreply.github.com>
2023-01-29 13:32:49 +0100

    Update install scripts.
    Signed-off-by: irfeazy <irfan.cela.5@gmail.com>

--- install-mongodb.sh
+++ install-mongodb.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/fish
-# Install the dependencies
+#!/usr/bin/env bash
+# Require sudo rights
 sudo echo ""
+
+# Remove MongoDB key
+sudo rm /usr/share/keyrings/mongodb-5.0.gpg
+
+# Remove latest MongoDB repository from your system's APT sources
+sudo rm /etc/apt/sources.list.d/mongodb-5.0.list
+
+# Install the dependencies
 sudo apt update
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 
@@ -13,10 +21,17 @@ echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-5.0.gpg] https
 # Update the local package database
 sudo apt-get update
 
-#MongoDb has no official build for ubuntu 22.04 at the moment.You can force the installation of libssl1.1 by adding the ubuntu 20.04 source, necessary to met the dependencies and install mongod-org:
-echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
-sudo apt-get update
-sudo apt-get install libssl1.1
+# MongoDb has no official build for ubuntu 22.04 at the moment.
+# You can force the installation of libssl1.1 by adding the ubuntu 20.04 source, necessary to met the dependencies and install mongod-org:
+REQUIRED_PKG="libssl1.1"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
+echo Checking for $REQUIRED_PKG: "$PKG_OK"
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+  sudo apt-get update
+  sudo apt-get install libssl1.1
+fi
 
 # Install the MongoDB with the following command
 sudo apt-get --install-recommends install mongodb-org
--- install-neo4j.sh
+++ install-neo4j.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/fish
+#!/usr/bin/env bash
 # Require sudo rights
 sudo echo ""
 
